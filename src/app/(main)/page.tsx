9a30b5de2f29207440dfe69c524e4c7a19cd85bb
Edlâneo Júlio <edlanioj@gmail.com>
2023-06-26 10:11:50 +0100

    feat: add search filter component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,6 @@
+import { Pencil } from 'lucide-react'
 import { Board } from '@/components/Board'
+import { SearchFilter } from '@/components/SearchFilter'
 import { getServerAuthSession } from '@/server/auth'
 
 export async function generateMetadata() {
@@ -10,7 +12,12 @@ export async function generateMetadata() {
 export default function Home() {
   return (
     <div className="space-y-8 px-10 py-12">
-      <h1 className="text-2xl font-bold leading-tight">Home</h1>
+      <h1 className="flex items-center gap-3 text-center text-xl font-bold text-zinc-800 xl:text-3xl">
+        Meus Bungles do dia
+        <Pencil className="text-zinc-400" />
+      </h1>
+
+      <SearchFilter />
       <Board />
     </div>
   )
---
+++ src/components/SearchFilter.tsx
@@ -0,0 +1,28 @@
+import { ListFilter, Search } from 'lucide-react'
+
+export function SearchFilter() {
+  return (
+    <section className="shadow-custom flex overflow-hidden rounded-md bg-white text-zinc-900">
+      <button
+        type="button"
+        className="flex items-center gap-2 bg-violet-600 px-4 py-3 text-violet-100 transition-colors hover:bg-violet-600/95"
+      >
+        <ListFilter />
+        Filtrar
+      </button>
+      <label
+        htmlFor="search"
+        className="flex w-full items-center gap-2 rounded-r-md bg-white px-4 ring-2"
+      >
+        <Search className="text-2xl" />
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="w-full text-sm placeholder:text-zinc-400 focus:outline-0"
+          placeholder="Busque por cards, assuntos ou responsáveis..."
+        />
+      </label>
+    </section>
+  )
+}
