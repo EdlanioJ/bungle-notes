ab4bc4d7f77d185e51f72248ca401e15a9757885
Edlâneo Júlio <edlanioj@gmail.com>
2023-06-26 21:12:29 +0100

    refactor: move sign in function to a handler

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -5,6 +5,14 @@ import { LucideGithub } from 'lucide-react'
 import { signIn } from 'next-auth/react'
 
 export function SignIn() {
+  const handleSignIn = async () => {
+    const result = await signIn('github', {
+      redirect: true,
+      callbackUrl: '/',
+    })
+
+    console.log(result)
+  }
   return (
     <div className=" rounded-lg bg-zinc-50 p-4 shadow-2xl">
       <h1 className="mb-6 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
@@ -17,7 +25,7 @@ export function SignIn() {
 
       <button
         type="button"
-        onClick={() => signIn('github')}
+        onClick={handleSignIn}
         className="flex w-64  items-center justify-center gap-2 rounded-lg bg-violet-600 py-3 text-zinc-50 transition-colors hover:bg-violet-500"
       >
         <LucideGithub />
