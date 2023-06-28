e4de246cf58e4adbb1d007cc65e051ef3c99ffaa
Edlâneo Júlio <edlanioj@gmail.com>
2023-06-28 22:39:33 +0100

    feat: add create task api route

--- src/server/api/routers/task.ts
+++ src/server/api/routers/task.ts
@@ -40,4 +40,33 @@ export const taskRouter = createTRPCRouter({
         select: { id: true },
       })
     }),
+
+  create: protectedProcedure
+    .input(
+      z.object({
+        content: z.string().nonempty(),
+        name: z.string().nonempty(),
+        date: z.date(),
+        tags: z.array(z.string()),
+        status: z.enum(['todo', 'inProgress', 'done']),
+        projectId: z.string().nonempty().cuid(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id
+      const task = await ctx.prisma.task.create({
+        data: {
+          content: input.content,
+          name: input.name,
+          date: input.date,
+          status: input.status,
+          projectId: input.projectId,
+          tags: input.tags.join(', '),
+          userId,
+        },
+        include: { project: { select: { name: true } } },
+      })
+
+      return TaskMapper.map(task)
+    }),
 })
