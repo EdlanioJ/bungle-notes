afd033b30aea34f4f4ed13a688e6cdf5e2dd116f
Edlâneo Júlio <edlanioj@gmail.com>
2023-06-29 00:15:34 +0100

    feat: ensure header change color on scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,52 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 import Image from 'next/image'
 import type { Session } from 'next-auth'
+import { Pencil } from 'lucide-react'
+import clsx from 'clsx'
 
 type Props = {
   user: Session['user']
 }
 
 export function Header({ user }: Props) {
+  const [isScrolled, setIsScrolled] = useState(false)
+  const [showText, setShowText] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const onScroll = (event: Event) => {
+      const main = event.target as HTMLElement
+      if (main.tagName.toLowerCase() === 'main') {
+        setIsScrolled(main.scrollTop > 0)
+        setShowText(main.scrollTop > 100)
+      }
+    }
+
+    window.addEventListener('scroll', onScroll, true)
+
+    return () => {
+      window.removeEventListener('scroll', onScroll, true)
+    }
+  }, [])
+
   return (
-    <header className="sticky top-0 z-50 flex w-full items-center justify-between px-10 pb-2 pt-4 ">
-      <div />
+    <header
+      className={clsx(
+        'sticky top-0 z-50 flex w-full items-center justify-between px-10 pb-2 pt-4 ',
+        { 'bg-white  shadow-custom': isScrolled },
+      )}
+    >
+      <div>
+        {pathname === '/' && showText && (
+          <h1 className="flex items-center justify-center gap-3 truncate text-center text-xl font-bold text-zinc-800 xl:text-2xl">
+            Meus Bungles do dia
+            <Pencil className="text-zinc-400" />
+          </h1>
+        )}
+      </div>
 
       <div className="group relative">
         <div className="relative h-11 w-11 overflow-hidden rounded-full border-4">
