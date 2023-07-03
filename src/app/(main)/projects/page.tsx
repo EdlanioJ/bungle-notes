f8954e69a75733f459c030ae0d00c35ec6f770df
Edlâneo Júlio <edlanioj@gmail.com>
2023-07-04 00:38:22 +0100

    feat: add table project table component from shadcn/ui

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -1,6 +1,6 @@
 import { CreateProjectModal } from '@/components/CreateProjectModal'
 import { ProjectHeader } from '@/components/ProjectHeader'
-import { ProjectList } from '@/components/ProjectList'
+import { ProjectTable } from '@/components/ProjectTable'
 
 export const metadata = {
   title: 'Projetos',
@@ -10,7 +10,7 @@ export default function Projects() {
   return (
     <div className="space-y-8 px-10 py-12 text-zinc-700 transition-all">
       <ProjectHeader />
-      <ProjectList />
+      <ProjectTable />
       <CreateProjectModal />
     </div>
   )
---
+++ src/components/ProjectTable.tsx
@@ -0,0 +1,59 @@
+import { api } from '@/utils/api'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './ui/table'
+import { Spinner } from './Spinner'
+import Link from 'next/link'
+
+export function ProjectTable() {
+  const { data: projects, isLoading } =
+    api.project.getUserFullProjects.useQuery()
+
+  if (isLoading) {
+    return (
+      <Spinner.Container>
+        <Spinner size="lg" />
+      </Spinner.Container>
+    )
+  }
+
+  if (!projects) return null
+  return (
+    <Table className="rounded-lg bg-white p-4">
+      <TableHeader>
+        <TableRow>
+          <TableHead>Nome do Projeto</TableHead>
+          <TableHead>Total de tarefas</TableHead>
+          <TableHead>A Fazer</TableHead>
+          <TableHead>Fazendo</TableHead>
+          <TableHead>Feito</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {projects.map((project) => {
+          const totalTask = Object.values(project.statusCount).reduce(
+            (acc, total) => acc + total,
+            0,
+          )
+
+          return (
+            <Link href={`/projects/${project.id}`} key={project.id}>
+              <TableRow>
+                <TableCell className="font-medium">{project.name}</TableCell>
+                <TableCell>{totalTask}</TableCell>
+                <TableCell>{project.statusCount.todo}</TableCell>
+                <TableCell>{project.statusCount.inProgress}</TableCell>
+                <TableCell>{project.statusCount.done}</TableCell>
+              </TableRow>
+            </Link>
+          )
+        })}
+      </TableBody>
+    </Table>
+  )
+}
