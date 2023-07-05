203f56735651dabb287ee46a29c8259d1b84b094
Edlâneo Júlio <edlanioj@gmail.com>
2023-07-05 22:14:44 +0100

    refactor: replace Disclosure to Popover in date input

--- src/components/Form/DateInput.tsx
+++ src/components/Form/DateInput.tsx
@@ -1,8 +1,8 @@
 import { ArrowLeft, ArrowRight, CalendarIcon } from 'lucide-react'
 
 import { Calendar } from '../Calendar'
-import { useState } from 'react'
-import { Disclosure } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import { Popover, Transition } from '@headlessui/react'
 import { DateFormatter } from '@/utils/date-formatter'
 
 export const monthNames = [
@@ -58,86 +58,95 @@ export function DateInput({ onChange, value }: Props) {
 
   return (
     <div className="w-full space-y-2 rounded-lg">
-      <Disclosure>
-        <Disclosure.Button className="flex w-full justify-between rounded-lg border px-3 py-2">
+      <Popover>
+        <Popover.Button className="flex w-full justify-between rounded-lg border px-3 py-2">
           <span>{DateFormatter.format(value)}</span>
           <CalendarIcon className="h-5 w-5 text-gray-400" />
-        </Disclosure.Button>
-        <Disclosure.Panel>
-          {({ close }) => (
-            <Calendar>
-              <Calendar.Header>
-                <Calendar.NavigationButton
-                  onClick={handlePreview}
-                  icon={ArrowLeft}
-                />
-                <Calendar.MonthDetails>
-                  {monthNames[currMonth]} {currYear}
-                </Calendar.MonthDetails>
-                <Calendar.NavigationButton
-                  onClick={handleNext}
-                  icon={ArrowRight}
-                />
-              </Calendar.Header>
-              <Calendar.Body>
-                <Calendar.WeekLabels />
-                <Calendar.MonthDays>
-                  {Array(firstDayOfMonth)
-                    .fill(null)
-                    .map((_, index) => {
-                      const day =
-                        lastDayOfLastMonth + index + 1 - firstDayOfMonth
-                      const date = new Date(currYear, currMonth - 1, day)
-                      return (
-                        <Calendar.Day
-                          date={date}
-                          disabled
-                          key={`${date.getTime()}`}
-                        />
-                      )
-                    })}
+        </Popover.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-1"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-1"
+        >
+          <Popover.Panel>
+            {({ close }) => (
+              <Calendar>
+                <Calendar.Header>
+                  <Calendar.NavigationButton
+                    onClick={handlePreview}
+                    icon={ArrowLeft}
+                  />
+                  <Calendar.MonthDetails>
+                    {monthNames[currMonth]} {currYear}
+                  </Calendar.MonthDetails>
+                  <Calendar.NavigationButton
+                    onClick={handleNext}
+                    icon={ArrowRight}
+                  />
+                </Calendar.Header>
+                <Calendar.Body>
+                  <Calendar.WeekLabels />
+                  <Calendar.MonthDays>
+                    {Array(firstDayOfMonth)
+                      .fill(null)
+                      .map((_, index) => {
+                        const day =
+                          lastDayOfLastMonth + index + 1 - firstDayOfMonth
+                        const date = new Date(currYear, currMonth - 1, day)
+                        return (
+                          <Calendar.Day
+                            date={date}
+                            disabled
+                            key={`${date.getTime()}`}
+                          />
+                        )
+                      })}
 
-                  {Array(lastDateOfMonth)
-                    .fill(null)
-                    .map((_, index) => {
-                      const day = index + 1
-                      const date = new Date(currYear, currMonth, day)
+                    {Array(lastDateOfMonth)
+                      .fill(null)
+                      .map((_, index) => {
+                        const day = index + 1
+                        const date = new Date(currYear, currMonth, day)
 
-                      const isSelected =
-                        value.getFullYear() === date.getFullYear() &&
-                        value.getMonth() === date.getMonth() &&
-                        value.getDate() === date.getDate()
+                        const isSelected =
+                          value.getFullYear() === date.getFullYear() &&
+                          value.getMonth() === date.getMonth() &&
+                          value.getDate() === date.getDate()
 
-                      const isDisabled =
-                        currYear < initDate.getFullYear() ||
-                        (currYear === initDate.getFullYear() &&
-                          currMonth < initDate.getMonth()) ||
-                        (currYear === initDate.getFullYear() &&
-                          currMonth === initDate.getMonth() &&
-                          day < initDate.getDate())
+                        const isDisabled =
+                          currYear < initDate.getFullYear() ||
+                          (currYear === initDate.getFullYear() &&
+                            currMonth < initDate.getMonth()) ||
+                          (currYear === initDate.getFullYear() &&
+                            currMonth === initDate.getMonth() &&
+                            day < initDate.getDate())
 
-                      function handleSelectDate(value: Date) {
-                        onChange(value)
-
-                        close()
-                      }
-                      return (
-                        <Calendar.Day
-                          date={date}
-                          isSelected={isSelected}
-                          disabled={isDisabled}
-                          handleSelectDate={handleSelectDate}
-                          key={`${date.getTime()}`}
-                        />
-                      )
-                    })}
-                </Calendar.MonthDays>
-              </Calendar.Body>
-              <div className="flex-1" />
-            </Calendar>
-          )}
-        </Disclosure.Panel>
-      </Disclosure>
+                        function handleSelectDate(value: Date) {
+                          onChange(value)
+                          close()
+                        }
+                        return (
+                          <Calendar.Day
+                            date={date}
+                            isSelected={isSelected}
+                            disabled={isDisabled}
+                            handleSelectDate={handleSelectDate}
+                            key={`${date.getTime()}`}
+                          />
+                        )
+                      })}
+                  </Calendar.MonthDays>
+                </Calendar.Body>
+                <div className="flex-1" />
+              </Calendar>
+            )}
+          </Popover.Panel>
+        </Transition>
+      </Popover>
     </div>
   )
 }
