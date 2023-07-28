4fa8f56311d085b2a715fabde88bf64006098292
Edlâneo Júlio <edlanioj@gmail.com>
2023-07-28 15:46:46 +0100

    refactor: add home page metadata

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,27 @@
 import { Pencil } from 'lucide-react'
 import { SearchFilter } from '@/components/SearchFilter'
-// import { getServerAuthSession } from '@/server/auth'
+import { getServerAuthSession } from '@/server/auth'
 import { FeedBoard } from '@/components/FeedBoard'
-import { ErrorBoundary } from 'react-error-boundary'
+import { Metadata } from 'next'
 
-export default function Home() {
+export const dynamic = 'force-dynamic'
+
+export async function generateMetadata(): Promise<Metadata> {
+  const session = await getServerAuthSession()
+  return {
+    title: session?.user.name,
+  }
+}
+
+export default async function Home() {
   return (
     <div className="space-y-8 px-10 py-12">
       <h1 className="flex items-center gap-3 text-center text-xl font-bold text-zinc-800 xl:text-3xl">
         Meus Bungles do dia
         <Pencil className="text-zinc-400" />
       </h1>
-
       <SearchFilter />
-      <ErrorBoundary fallback={<p>Something went wrong</p>}>
-        <FeedBoard />
-      </ErrorBoundary>
+      <FeedBoard />
     </div>
   )
 }
