d8d7b04b08370aa652dfc5ebf3e9498041b05334
Edlâneo Júlio <edlanioj@gmail.com>
2023-08-05 23:10:57 +0100

    add trpc route to get project and task status count

--- src/server/api/routers/stats.ts
+++ src/server/api/routers/stats.ts
@@ -1,3 +1,22 @@
-import { createTRPCRouter } from '../trpc'
+import { createTRPCRouter, protectedProcedure } from '../trpc'
 
-export const statsRouter = createTRPCRouter({})
+export const statsRouter = createTRPCRouter({
+  projectAndTaskStatusCount: protectedProcedure.query(async ({ ctx }) => {
+    const userId = ctx.session.user.id
+    const [projectCount, taskStatusCountRes] = await Promise.all([
+      ctx.prisma.project.count({ where: { deletedAt: null, userId } }),
+      ctx.prisma.task.groupBy({
+        by: ['status'],
+        where: { deletedAt: null, userId },
+        _count: { status: true },
+        orderBy: { _count: { status: 'desc' } },
+      }),
+    ])
+
+    const taskStatusCount: StatusCount = taskStatusCountRes.reduce(
+      (acc, value) => ({ ...acc, [value.status]: value._count.status }),
+      { done: 0, inProgress: 0, todo: 0 },
+    )
+    return { projectCount, taskStatusCount }
+  }),
+})
