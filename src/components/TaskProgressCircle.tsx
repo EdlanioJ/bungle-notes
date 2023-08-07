be970f7399d904dc59837720e41d9404f74bf168
Edlâneo Júlio <edlanioj@gmail.com>
2023-08-07 22:34:51 +0100

    fix task progress circle component

--- src/components/TaskProgressCircle.tsx
+++ src/components/TaskProgressCircle.tsx
@@ -20,57 +20,36 @@ const getPercentages = (statusCount: StatusCount): StatusCount => {
   return percentages
 }
 
-type PathProps = {
-  size: number
-  percentage: number
-  percentages: StatusCount
-  index: number
-  status: string
-}
-
-function Path({ size, percentage, index, percentages, status }: PathProps) {
+export function TaskProgressCircle({ size = 150, data }: Props) {
   const STROKE_WIDTH = 10
   const radius = (size - STROKE_WIDTH) / 2
-  const endAngle =
-    (index > 0 ? Object.values(percentages)[index - 1] : 0) ??
-    0 + percentage * 3.6
-  const largeArcFlag = percentage > 50 ? 1 : 0
-  const startAngle = endAngle - percentage * 3.6
-  const x1 = size / 2 + radius * Math.cos((startAngle - 90) * (Math.PI / 180))
-  const y1 = size / 2 + radius * Math.sin((startAngle - 90) * (Math.PI / 180))
-  const x2 = size / 2 + radius * Math.cos((endAngle - 90) * (Math.PI / 180))
-  const y2 = size / 2 + radius * Math.sin((endAngle - 90) * (Math.PI / 180))
-
-  return (
-    <path
-      key={status}
-      d={`M ${x1} ${y1} A ${radius} ${radius} 0 ${largeArcFlag} 1 ${x2} ${y2}`}
-      fill="none"
-      className={cn({
-        'stroke-green-600': status === 'done',
-        'stroke-red-500': status === 'todo',
-        'stroke-amber-500': status === 'inProgress',
-      })}
-      strokeWidth={STROKE_WIDTH}
-      strokeLinecap="round"
-    />
-  )
-}
-
-export function TaskProgressCircle({ size = 150, data }: Props) {
   const percentages = getPercentages(data)
+  let startAngle = 0
   return (
     <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
-      {Object.entries(percentages).map(([status, percentage], index) => (
-        <Path
-          index={index}
-          percentage={percentage}
-          percentages={percentages}
-          size={size}
-          status={status}
-          key={status}
-        />
-      ))}
+      {Object.entries(percentages).map(([status, percentage]) => {
+        const endAngle = startAngle + percentage * 3.6 // 3.6 degrees per percentage
+        const largeArcFlag = percentage > 50 ? 1 : 0 // Use a large arc if percentage is greater than 50
+        const x1 = 100 + radius * Math.cos((startAngle - 90) * (Math.PI / 180))
+        const y1 = 100 + radius * Math.sin((startAngle - 90) * (Math.PI / 180))
+        const x2 = 100 + radius * Math.cos((endAngle - 90) * (Math.PI / 180))
+        const y2 = 100 + radius * Math.sin((endAngle - 90) * (Math.PI / 180))
+
+        startAngle = endAngle - 0.1
+        return (
+          <path
+            key={status}
+            d={`M ${x1} ${y1} A ${radius} ${radius} 0 ${largeArcFlag} 1 ${x2} ${y2}`}
+            fill="none"
+            strokeWidth={STROKE_WIDTH}
+            className={cn({
+              'stroke-green-600': status === 'done',
+              'stroke-red-500': status === 'todo',
+              'stroke-amber-500': status === 'inProgress',
+            })}
+          />
+        )
+      })}
     </svg>
   )
 }
