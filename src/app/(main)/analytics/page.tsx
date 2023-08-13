7a02ed2e78e77b7188d8594b056bf4f0efeaf5fa
Edlâneo Júlio <edlanioj@gmail.com>
2023-08-13 19:37:05 +0100

    refactor analytics page

--- src/app/(main)/analytics/page.tsx
+++ src/app/(main)/analytics/page.tsx
@@ -1,8 +1,6 @@
 import { Heading } from '@/components/Heading'
-import { ProgressCircle } from '@/components/ProgressCircle'
 import { ProgressCircleSection } from '@/components/ProgressCircleSection'
 import { ssgHelper } from '@/server/helpers/ssgHelper'
-import { cn } from '@/utils/cn'
 
 async function getData() {
   const ssg = await ssgHelper()
@@ -16,14 +14,6 @@ export const metadata = {
   title: 'Relatórios',
 }
 
-const statusToText: {
-  [key in TaskStatus]: string
-} = {
-  todo: 'A fazer',
-  inProgress: 'Fazendo',
-  done: 'Feito',
-}
-
 export default async function Analytics() {
   const projectAndTaskStatusCount = await getData()
   return (
@@ -31,37 +21,6 @@ export default async function Analytics() {
       <Heading>Relatórios</Heading>
 
       <ProgressCircleSection data={projectAndTaskStatusCount} />
-      <div className="flex w-full flex-col gap-4 overflow-hidden rounded-xl bg-white p-4 shadow-2xl transition-all duration-300 hover:shadow-none">
-        <div className="flex w-full items-center justify-center">
-          <ProgressCircle data={projectAndTaskStatusCount} />
-        </div>
-        <div className="flex justify-around gap-2">
-          {Object.entries(projectAndTaskStatusCount.taskStatusCount).map(
-            ([status, count]) => (
-              <div
-                key={status}
-                className="flex flex-1 flex-col items-center justify-center gap-1"
-              >
-                <span className="text-2xl font-bold text-zinc-700">
-                  {count}
-                </span>
-                <div className="flex items-center gap-1">
-                  <div
-                    className={cn('h-2 w-2 rounded-full shadow-2xl', {
-                      'bg-red-400': status === 'todo',
-                      'bg-amber-400': status === 'inProgress',
-                      'bg-green-600': status === 'done',
-                    })}
-                  />
-                  <span className="text-xs font-semibold text-zinc-400">
-                    {statusToText[status as TaskStatus]}
-                  </span>
-                </div>
-              </div>
-            ),
-          )}
-        </div>
-      </div>
     </div>
   )
 }
