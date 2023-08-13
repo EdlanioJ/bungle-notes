76de53d15c1883b28b0b4602b723f49d289839d5
Edlâneo Júlio <edlanioj@gmail.com>
2023-08-13 11:11:50 +0100

    fix progress circle section component

--- src/components/ProgressCircleSection.tsx
+++ src/components/ProgressCircleSection.tsx
@@ -3,12 +3,38 @@
 import { cn } from '@/utils/cn'
 import { Cell, Pie, PieChart } from 'recharts'
 
+const SIZE = 150
 const statusToText: {
   [key in TaskStatus]: string
 } = {
   todo: 'A fazer',
   inProgress: 'Fazendo',
   done: 'Feito',
+}
+
+type CenterProps = {
+  projectCount: number
+}
+function Center({ projectCount }: CenterProps) {
+  return (
+    <g transform={`translate(${SIZE / 2},${SIZE / 2})`}>
+      <text
+        textAnchor="middle"
+        fontSize="48"
+        className="fill-zinc-700 font-bold"
+      >
+        {projectCount}
+      </text>
+      <text
+        y="30"
+        textAnchor="middle"
+        fontSize="16"
+        className="fill-zinc-400 font-semibold"
+      >
+        {projectCount <= 1 ? 'Projeto' : 'Projetos'}
+      </text>
+    </g>
+  )
 }
 
 type Props = {
@@ -27,11 +53,11 @@ export function ProgressCircleSection({ data }: Props) {
   return (
     <section className="flex w-full flex-col gap-4 overflow-hidden rounded-xl bg-white p-4 shadow-2xl transition-all duration-300 hover:shadow-none">
       <div className="flex w-full items-center justify-center">
-        <PieChart width={150} height={150}>
+        <PieChart width={SIZE} height={SIZE}>
           <Pie
             data={statusData}
-            innerRadius={65}
-            outerRadius={75}
+            innerRadius={SIZE / 2 - 10}
+            outerRadius={SIZE / 2}
             fill="none"
             dataKey="count"
           >
@@ -46,6 +72,7 @@ export function ProgressCircleSection({ data }: Props) {
               />
             ))}
           </Pie>
+          <Center projectCount={data.projectCount} />
         </PieChart>
       </div>
       <div className="flex justify-around gap-2">
