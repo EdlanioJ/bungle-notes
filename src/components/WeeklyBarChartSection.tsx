09e8acb559f7f864a78b54d7c775eddf46c517f4
Edlâneo Júlio <edlanioj@gmail.com>
2023-08-17 10:04:35 +0100

    fix weekly bar chart section component

--- src/components/WeeklyBarChartSection.tsx
+++ src/components/WeeklyBarChartSection.tsx
@@ -1,31 +1,29 @@
 'use client'
 
-import { Bar, BarChart, ResponsiveContainer, XAxis } from 'recharts'
+import { Bar, BarChart, XAxis } from 'recharts'
 
 const data = [
-  { weekDay: 'Dom', value: 30 },
-  { weekDay: 'Seg', value: 25 },
-  { weekDay: 'Ter', value: 3 },
-  { weekDay: 'Qua', value: 15 },
-  { weekDay: 'Qui', value: 12 },
-  { weekDay: 'Sex', value: 7 },
-  { weekDay: 'Sab', value: 45 },
+  { name: 'Dom', value: 30 },
+  { name: 'Seg', value: 25 },
+  { name: 'Ter', value: 3 },
+  { name: 'Qua', value: 15 },
+  { name: 'Qui', value: 12 },
+  { name: 'Sex', value: 7 },
+  { name: 'Sab', value: 45 },
 ]
 
 export function WeeklyBarChartSection() {
   return (
     <section className="flex w-full flex-col gap-4 overflow-hidden rounded-xl bg-white p-4 shadow-2xl transition-all duration-300 hover:shadow-none">
-      <ResponsiveContainer width={'100%'} height={'100%'}>
-        <BarChart width={320} height={180} data={data}>
-          <Bar dataKey="value" className="fill-blue-500" background />
-          <XAxis
-            dataKey="weekDay"
-            axisLine={false}
-            tickLine={false}
-            className="text-xs font-bold text-zinc-500"
-          />
-        </BarChart>
-      </ResponsiveContainer>
+      <BarChart width={320} height={180} data={data}>
+        <Bar dataKey="value" className="fill-blue-500" background />
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          tickLine={false}
+          className="text-xs font-bold text-zinc-500"
+        />
+      </BarChart>
     </section>
   )
 }
