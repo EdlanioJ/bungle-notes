83a2e85db5816b85b20f3cb995f3e64c8defda66
Edlâneo Júlio <edlanioj@gmail.com>
2023-08-21 08:53:12 +0100

    update settings page

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -7,7 +7,10 @@ export const metadata = {
 export default function Settings() {
   return (
     <div className="flex h-full w-full flex-col gap-6 md:pb-6">
-      <Heading>Ajustes</Heading>
+      <Heading>Configurações de Conta</Heading>
+      <section className="flex w-full flex-col gap-2 overflow-hidden rounded-xl bg-white p-4 shadow-2xl transition-all duration-300 hover:shadow-none">
+        <strong className="text-xs text-zinc-700">Meu Perfil</strong>
+      </section>
     </div>
   )
 }
